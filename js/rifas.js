2e5608ccea6bb8827af9ab9e0b4d5ef6b9170a56
Victoria Kropp <victoriakropp@yahoo.com.ar>
2023-10-05 15:00:39 -0300

    primer intento

---
+++ js/rifas.js
@@ -0,0 +1,27 @@
+function calcTotal() {
+    const precio=500
+    console.log("Ingreso a calcTotal")
+
+    let cant=document.getElementById("cant").value
+    console.log(cant)
+
+    if (cant>=1) {
+        let total=cant*precio
+        console.log(total)
+
+        let desc=document.getElementById("desc").value
+        total=total - (total*desc/100)
+    
+        // document.getElementById("total").style.color="red"
+        document.getElementById("total").className="bg-info p-1"
+        document.getElementById("total").innerHTML=total
+            
+    } else {
+        document.getElementById("total").className="bg-danger p-1"
+        document.getElementById("total").innerHTML="La cantidad de tickets debe ser mayor o igual a 1"
+        
+    }
+
+
+    
+}
